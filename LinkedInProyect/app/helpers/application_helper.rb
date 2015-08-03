5a6bfa42fa8c3a0103c3e0a911e8f3126c02aa47
William Hedén <pwheden@gmail.com>
2015-08-03 20:27:29 +0200

    change of text and images
    change of text and images

--- LinkedInProyect/app/helpers/application_helper.rb
+++ LinkedInProyect/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   #If no specific title is given on a certain page, the "base_title" is shown on top of the page instead.
   #Use this to provide a specific title in the 'views' scripts: <% provide(:title, "Home") %>
   def full_title(page_title = '')
-    base_title = "Mining LinkedIn"
+    base_title = "Lettuce"
     if page_title.empty?
       base_title
     else
